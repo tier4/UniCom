ce4541edf1541aa7d851d8ed62cc68bae620b5b2
Masaya Kataoka <ms.kataoka@gmail.com>
2019-12-11 17:27:20 +0900

    add setTopic function

--- Assets/Scripts/UniCom/Publisher.cs
+++ Assets/Scripts/UniCom/Publisher.cs
@@ -10,14 +10,44 @@ namespace UniCom
         public Publisher(string topic)
         {
             _topic = topic;
+            _topic_initialized = true;
+        }
+
+        public Publisher()
+        {
+            _topic = null;
+            _topic_initialized = false;
+        }
+
+        public void SetTopic(string topic)
+        {
+            if (!_topic_initialized)
+            {
+                _topic = topic;
+                _topic_initialized = true;
+            }
+            else
+            {
+                Debug.LogWarning("Topic name was tried overwrited. Please chekc your code.");
+            }
         }
 
         public void Publish(T data)
         {
-            ComData<T> com_data = new ComData<T>(_topic,data);
+            if (!_topic_initialized)
+            {
+                return;
+            }
+            if (string.IsNullOrEmpty(_topic))
+            {
+                Debug.LogWarning("Topic name is not valid.");
+                return;
+            }
+            ComData<T> com_data = new ComData<T>(_topic, data);
             MessageBroker.Default.Publish<ComData<T>>(com_data);
         }
 
         private string _topic;
+        private bool _topic_initialized = false;
     }
 }
--- Assets/Scripts/UniCom/Subscriber.cs
+++ Assets/Scripts/UniCom/Subscriber.cs
@@ -10,17 +10,18 @@ namespace UniCom
     {
         public Subscriber(string topic, Action<T> func)
         {
+            _topic_initialized = true;
             _topic = topic;
             _func = func;
             MessageBroker.Default.Receive<ComData<T>>().Subscribe(
                 x =>
                 {
-                    if(x.hash != typeof(T).GetHashCode())
+                    if (x.hash != typeof(T).GetHashCode())
                     {
                         Debug.LogError("Hashcode does not match. Chekck data type");
                         return;
                     }
-                    if(x.topic == _topic)
+                    if (x.topic == _topic)
                     {
                         _func(x.data);
                     }
@@ -29,8 +30,24 @@ namespace UniCom
             return;
         }
 
+        public Subscriber(Action<T> func)
+        {
+            _func = func;
+            _topic_initialized = false;
+        }
+
+        public void SetTopic(string topic)
+        {
+            if (_topic_initialized)
+            {
+                _topic = topic;
+                _topic_initialized = true;
+            }
+        }
+
         private string _topic;
         private ComData<T> _data;
-        Action<T> _func;
+        private Action<T> _func;
+        private bool _topic_initialized = false;
     }
 }
